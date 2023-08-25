0268a752ba5590b9561865c87bc13dca165b318d
marcinpm <marcinpm69@gmail.com>
2023-08-25 18:03:53 +0200

    rozpoczecie zadania

---
+++ main.js
@@ -0,0 +1,17 @@
+let txt1 = 'Zmienna 1';
+
+let txt2 = 'Zmienna 2';
+
+let txt3 = 'Zmienna 3';
+
+console.log(txt1);
+
+console.log(txt2);
+
+console.log(txt3);
+
+let array = [txt1, txt2, txt3,];
+
+console.log(array);
+
+
